1a4fb8e9d3ce3e6f21037b26260d779196a6fcaa
Kai <kaisoapbox@gmail.com>
2025-01-23 17:01:51 -0800

    filter for following

--- popup/popup.js
+++ popup/popup.js
@@ -2,10 +2,18 @@
 document.getElementById("scraping-btn").addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     console.log("hi");
-    chrome.tabs.sendMessage(tabs[0].id, { msg: "get-mutuals" });
+    chrome.tabs.sendMessage(tabs[0].id, {
+      msg: "get-mutuals",
+      username: "calvin__liang",
+    });
   });
 });
 
 document.getElementById("play-btn").addEventListener("click", function () {
   chrome.tabs.create({ url: chrome.runtime.getURL("page/index.html") });
+});
+
+// receive mutual followers data from page
+chrome.runtime.onMessage.addListener((data) => {
+  console.log(data);
 });
--- scripts/getMutualFollowers.js
+++ scripts/getMutualFollowers.js
@@ -1,19 +1,12 @@
 chrome.runtime.onMessage.addListener((data) => {
   if (data.msg === "get-mutuals") {
     console.log("hi");
-    getMutuals("calvin__liang");
+    getMutuals(data.username);
   }
 });
 
 // adapted from https://stackoverflow.com/a/74133719
 async function getMutuals(username) {
-  /**
-   * Initialized like this so we can still run it from browsers, but also use typescript on a code editor for intellisense.
-   */
-  let mutuals = [{ username: "", full_name: "", id: "", profile_pic_url: "" }];
-
-  mutuals = [];
-
   try {
     console.log(`Process started! Give it a couple of seconds`);
 
@@ -27,7 +20,9 @@ async function getMutuals(username) {
       .map((u) => u.user)
       .filter((u) => u.username === username)[0].pk;
 
-    const mutualQueryRes = await fetch(
+    // we're only fetching the first mutuals that the query returns
+    // during testing it's always fit into one query and returns plenty of mutual followers
+    const mutualFollowerQueryRes = await fetch(
       `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=` +
         encodeURIComponent(
           JSON.stringify({
@@ -39,23 +34,74 @@ async function getMutuals(username) {
           })
         )
     );
-    const mutualQueryJson = await mutualQueryRes.json();
-    mutuals = mutualQueryJson.data.user.edge_mutual_followed_by.edges.map(
-      ({ node }) => {
-        return {
-          username: node.username,
-          full_name: node.full_name,
-          id: node.id,
-          profile_pic_url: node.profile_pic_url,
-        };
-      }
-    );
+    const mutualFollowerQueryJson = await mutualFollowerQueryRes.json();
+    const mutualFollowers =
+      mutualFollowerQueryJson.data.user.edge_mutual_followed_by.edges.map(
+        ({ node }) => {
+          return {
+            username: node.username,
+            full_name: node.full_name,
+            id: node.id,
+            profile_pic_url: node.profile_pic_url,
+          };
+        }
+      );
 
-    console.log({ mutuals });
+    console.log(mutualFollowers);
 
-    console.log(
-      `Process is done: Type 'copy(mutuals)' in the console and paste it into a text editor to take a look at it'`
-    );
+    // get friend's following
+    // initialized like this so it still runs in browsers, but also still has intellisense
+    let followings = [
+      { username: "", full_name: "", id: "", profile_pic_url: "" },
+    ];
+    followings = [];
+    after = null;
+    has_next = true;
+
+    while (has_next) {
+      await fetch(
+        `https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=` +
+          encodeURIComponent(
+            JSON.stringify({
+              id: userId,
+              include_reel: true,
+              fetch_mutual: true,
+              first: 50,
+              after: after,
+            })
+          )
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          has_next = res.data.user.edge_follow.page_info.has_next_page;
+          after = res.data.user.edge_follow.page_info.end_cursor;
+          followings = followings.concat(
+            res.data.user.edge_follow.edges.map(({ node }) => {
+              return {
+                username: node.username,
+                full_name: node.full_name,
+                id: node.id,
+                profile_pic_url: node.profile_pic_url,
+              };
+            })
+          );
+        });
+    }
+
+    console.log({ followings });
+
+    // filter for mutuals that the friend is following
+    const filteredMutuals = mutualFollowers.filter((follower) => {
+      return followings.find(
+        (following) => follower.username === following.username
+      );
+    });
+    console.log(filteredMutuals);
+
+    // send extension filtered mutuals from the page
+    chrome.runtime.sendMessage(filteredMutuals);
+
+    console.log("Done with getting mutuals!");
   } catch (err) {
     console.log({ err });
   }
