dadc141aa0025c629c00728d0d3054f9eb2a561b
Kai <kaisoapbox@gmail.com>
2025-01-28 19:23:24 -0800

    enter submits input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import { useState } from "react";
 import findMutualFollowers from "./components/findMutualFollowers.js";
-import {ProfileCard} from "./components/ProfileCard.jsx";
+import { ProfileCard } from "./components/ProfileCard.jsx";
 
 export default function App() {
   const [username, setUsername] = useState("");
@@ -9,90 +9,131 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
 
   function handleClick() {
-      setIsLoading(true);
-      findMutualFollowers(username, handleResponse);
+    setIsLoading(true);
+    findMutualFollowers(username, handleResponse);
   }
 
   function handleResponse(data) {
-      let newProfiles = data
-          .slice(0, 24)
-          .map(e => { return {...e, enabled: true} });
-      setProfiles(newProfiles);
-      setIsLoading(false);
-      setIsGaming(true);
+    setProfiles(
+      data.map((e, i) => {
+        return { ...e, selected: i < 24, enabled: true };
+      })
+    );
+    setIsLoading(false);
+    setIsGaming(true);
   }
 
-    function toggleCard(index) {
-        setProfiles((prevProfiles) =>
-            prevProfiles.map((profile, i) =>
-                i === index ? { ...profile, enabled: !profile.enabled } : profile
-            )
-        );
-    }
+  function toggleCard(index) {
+    setProfiles((prevProfiles) =>
+      prevProfiles.map((profile, i) =>
+        i === index ? { ...profile, enabled: !profile.enabled } : profile
+      )
+    );
+  }
 
-    function resetAll() {
-        setProfiles((prevProfiles) =>
-            prevProfiles.map((profile) => {
-                return {...profile, enabled: true}
-            })
-        )
-    }
+  function resetAll() {
+    setProfiles((prevProfiles) =>
+      prevProfiles.map((profile) => {
+        return { ...profile, enabled: true };
+      })
+    );
+  }
 
-    function returnToMain() {
-        setIsGaming(false);
-        setUsername("");
-        setProfiles([]);
-        setIsLoading(false);
-    }
+  function returnToMain() {
+    setIsGaming(false);
+    setUsername("");
+    setProfiles([]);
+    setIsLoading(false);
+  }
 
-    return (
+  return (
     <>
-        {!isGaming
-        ? <div className={"m-4 w-full flex flex-col gap-4 items-center justify-center"}>
-                <h1>Instagram Guess Who</h1>
-                <div className={"flex gap-4 items-center"}>
-                    <input
-                        className={"h-8 border border-2 border-gray"}
-                        type="text"
-                        onChange={(event) => {
-                        setUsername(event.target.value);
-                        }}
-                        disabled={isLoading}
-                    />
-                    <button
-                        className={`${isLoading ? "bg-blue-300" : "bg-blue-500 hover:bg-blue-700"} 
+      {!isGaming ? (
+        <div
+          className={
+            "m-4 w-full flex flex-col gap-4 items-center justify-center"
+          }
+        >
+          <h1>Instagram Guess Who</h1>
+          <div className={"flex gap-4 items-center"}>
+            <input
+              className={"h-8 border border-2 border-gray"}
+              type="text"
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  handleClick();
+                }
+              }}
+              disabled={isLoading}
+            />
+            <button
+              className={`${
+                isLoading ? "bg-blue-300" : "bg-blue-500 hover:bg-blue-700"
+              } 
                         border-none text-white font-bold py-2 px-4 rounded`}
-                        onClick={handleClick}
-                        disabled={isLoading}
-                    >
-                        {!isLoading ? "Launch" : "Loading..."}
-                    </button>
-                </div>
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              {!isLoading ? "Launch" : "Loading..."}
+            </button>
+          </div>
         </div>
-        : <div className={"m-4 flex flex-col items-start"}>
-            <div className={"mb-4 w-full flex flex-row justify-between items-center"}>
-                <div className={"flex flex-row gap-4 items-center"}>
-                    <button className={"p-0 border-none"} onClick={returnToMain}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             className="bi bi-arrow-left" viewBox="0 0 16 16">
-                            <path fillRule="evenodd"
-                                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-                        </svg>
-                    </button>
-                    <h1 className={"mb-1"}>Instagram Guess Who</h1>
-                    <p className={"mb-1 ml-2 text-lg text-gray-400"}>Playing with {username}</p>
-                </div>
-                <button className={"bg-gray-200 hover:bg-gray-300 border-none text-[1rem]"} onClick={() => resetAll()}>
-                    Reset All
-                </button>
+      ) : (
+        <div className={"m-4 flex flex-col items-start"}>
+          <div
+            className={"mb-4 w-full flex flex-row justify-between items-center"}
+          >
+            <div className={"flex flex-row gap-4 items-center"}>
+              <button className={"p-0 border-none"} onClick={returnToMain}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                  />
+                </svg>
+              </button>
+              <h1 className={"mb-1"}>Instagram Guess Who</h1>
+              <p className={"mb-1 ml-2 text-lg text-gray-400"}>
+                Playing with {username}
+              </p>
             </div>
-            <div className={"grid grid-cols-8 gap-4"}>
-                {profiles.map((profile, i) =>
-                    <ProfileCard profile={profile} onClick={() => toggleCard(i)}/>
-                )}
-            </div>
-            </div>
-        }
+            <button
+              className={
+                "bg-gray-200 hover:bg-gray-300 border-none text-[1rem] text-black"
+              }
+              onClick={() => resetAll()}
+            >
+              Select Users
+            </button>
+            <button
+              className={
+                "bg-gray-200 hover:bg-gray-300 border-none text-[1rem] text-black"
+              }
+              onClick={() => resetAll()}
+            >
+              Reset All
+            </button>
+          </div>
+          <div className={"grid grid-cols-8 gap-4"}>
+            {profiles
+              .filter((profile) => profile.selected)
+              .map((profile, i) => (
+                <ProfileCard profile={profile} onClick={() => toggleCard(i)} />
+              ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,22 +1,32 @@
 export function ProfileCard(props) {
-    return <div
-        className={`${props.profile.enabled
-            ? "bg-blue-400 hover:bg-blue-200"
-            : "bg-gray-200 hover:bg-gray-400"}
+  return (
+    <div
+      className={`${
+        props.profile.enabled
+          ? "bg-blue-400 hover:bg-blue-200"
+          : "bg-gray-200 hover:bg-gray-400"
+      }
             p-4 rounded-md flex flex-col items-center cursor-pointer
             transition-all duration-200`}
-        onClick={props.onClick}
+      onClick={props.onClick}
     >
-        <img alt={props.profile.username} src={props.profile.profile_pic} className={"rounded-full mb-1"}/>
-        <a
-            className={`hover:underline ${props.profile.enabled
-                ? "text-white hover:text-white"
-                : "text-black hover:text-black"}`}
-            href={`https://instagram.com/${props.profile.username}`}
-            target={"_blank"}
-        >
-            {props.profile.username} <br/>
-            {props.profile.full_name}
-        </a>
-    </div>;
+      <img
+        alt={props.profile.username}
+        src={props.profile.profile_pic}
+        className={"rounded-full mb-1"}
+      />
+      <a
+        className={`hover:underline text-center ${
+          props.profile.enabled
+            ? "text-white hover:text-white"
+            : "text-black hover:text-black"
+        }`}
+        href={`https://instagram.com/${props.profile.username}`}
+        target={"_blank"}
+      >
+        {props.profile.username} <br />
+        {props.profile.full_name}
+      </a>
+    </div>
+  );
 }
