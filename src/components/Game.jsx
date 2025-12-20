7f42bdd690fb1ffb235988b3f3f85b58d5b6d83e
jentaruno <jennifertaruno@gmail.com>
2025-12-19 21:34:00 -0800

    basic menu button

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,9 @@ import { SelectionModal } from "./SelectionModal.jsx";
 import { BackButton } from "./BackButton.jsx";
 import { HelpModal } from "./HelpModal.jsx";
 import { QuestionModal } from "./QuestionModal.jsx";
+import { LiaUserEditSolid } from "react-icons/lia";
+import { PiDiceThree } from "react-icons/pi";
+import { TbReload } from "react-icons/tb";
 
 export default function Game({
   friend,
@@ -14,9 +17,61 @@ export default function Game({
   conn,
   toggleCard,
 }) {
+  const [showMoreMenu, setShowMoreMenu] = useState(false);
   const [showQuestionModal, setShowQuestionModal] = useState(false);
   const [showHelpModal, setShowHelpModal] = useState(false);
   const [showSelectionModal, setShowSelectionModal] = useState(false);
+
+  const showMoreButton = (
+    <div className="relative inline-block">
+      <button
+        className={"p-0 border-none focus:outline-none bg-transparent"}
+        onClick={() => setShowMoreMenu(!showMoreMenu)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          class="bi bi-three-dots"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+        </svg>
+      </button>
+      {showMoreMenu && (
+        <div
+          className="origin-top-right absolute right-0 z-10 mt-2 w-56
+                    rounded-md shadow-lg bg-white dark:bg-gray-800 focus:outline-none"
+          role="menu"
+        >
+          <div className="py-1" role="none">
+            <a
+              className="flex flex-row gap-2 block px-4 py-2 cursor-pointer text-black dark:text-white hover:text-black dark:hover:text-white"
+              role="menuitem"
+            >
+              <LiaUserEditSolid size={24} />
+              Select Users
+            </a>
+            <a
+              className="flex flex-row gap-2 block px-4 py-2 cursor-pointer text-black dark:text-white hover:text-black dark:hover:text-white"
+              role="menuitem"
+            >
+              <PiDiceThree size={24} />
+              Random Question
+            </a>
+            <a
+              className="flex flex-row gap-2 block px-4 py-2 cursor-pointer text-black dark:text-white hover:text-black dark:hover:text-white"
+              role="menuitem"
+            >
+              <TbReload size={24} />
+              Reset All
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 
   return (
     <div className={"m-4 flex flex-col items-start"}>
@@ -39,6 +94,7 @@ export default function Game({
           </p>
         </div>
         <div className={"flex flex-row gap-4 md:items-center"}>
+          {showMoreButton}
           <button
             className={"p-0 border-none focus:outline-none bg-transparent"}
             onClick={() => setShowHelpModal(true)}
@@ -107,7 +163,9 @@ export default function Game({
           hideModal={() => setShowSelectionModal(false)}
         />
       )}
-      {showQuestionModal && <QuestionModal hideModal={() => setShowQuestionModal(false)} />}
+      {showQuestionModal && (
+        <QuestionModal hideModal={() => setShowQuestionModal(false)} />
+      )}
       {showHelpModal && <HelpModal hideModal={() => setShowHelpModal(false)} />}
     </div>
   );
